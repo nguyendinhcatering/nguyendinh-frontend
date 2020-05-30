34a0323c6666348ceff421bc47201d6c68597e82
Kevin Nguyen <k.nguyen0125@gmail.com>
2020-05-30 15:26:53 +0700

    feat: implemented basic card

---
+++ components/ui/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "theme-ui";
+import cn from "classnames";
+
+const Card = ({ className, children, ...props }) => {
+  return (
+    <Box className={cn("shadow-elevation-12 bg-white", className)} {...props}>
+      {children}
+    </Box>
+  );
+};
+
+export default Card;
