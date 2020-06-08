947e4cada74a46c8fc0103bcfced068d9f2f12c8
Kevin Nguyen <k.nguyen0125@gmail.com>
2020-06-08 22:45:19 +0700

    feat: update utils

--- utils/getImageSrc.js
+++ utils/getImageSrc.js
@@ -1,7 +1,15 @@
-export const getImageUrl = (image) => {
+export const getImageUrl = (image, quality, useOriginal = false) => {
   if (!image) return null;
 
   let src = image.url;
+
+  if (quality) {
+    return process.env.NEXT_PUBLIC_BACKEND_URL + image.formats[quality].url;
+  }
+
+  if (useOriginal) {
+    return process.env.NEXT_PUBLIC_BACKEND_URL + src;
+  }
 
   if (image.formats) {
     if (image.formats.large) src = image.formats.large.url;
