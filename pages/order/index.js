fe6060be0afd0c31342a91ad8830e3c8ae38526a
Kevin Nguyen <k.nguyen0125@gmail.com>
2020-08-11 23:27:30 +0700

    fix: order page

--- pages/order/index.js
+++ pages/order/index.js
@@ -2,67 +2,11 @@ import React from "react";
 import DefaultLayout from "../../components/layout/DefaultLayout";
 import Page from "../../components/layout/Page";
 import API from "../../utils/api";
-import { kebabCase, sortBy } from "lodash";
 
-const Order = ({ layout, page, foodPresetTypes }) => {
-  const convertPresetTypesToSection = () => {
-    return sortBy(foodPresetTypes, ["order"]).map((presetType, index) => {
-      const text = JSON.parse(presetType.description);
-
-      text.blocks = [
-        {
-          type: "header",
-          data: {
-            alignment: "left",
-            text: presetType.name,
-            level: "2",
-          },
-        },
-        {
-          type: "paragraph",
-          data: {
-            alignment: "left",
-            text: "&nbsp;",
-          },
-        },
-        ...text.blocks,
-        {
-          type: "paragraph",
-          data: {
-            alignment: "left",
-            text: "&nbsp;",
-          },
-        },
-        {
-          type: "linkButton",
-          data: {
-            url: `/order/${kebabCase(presetType.type)}/${presetType.slug}`,
-            title: "Xem thêm",
-          },
-        },
-      ];
-
-      return {
-        id: Math.random(),
-        type: "SECTION",
-        mediaPlacement: index % 2 === 0 ? "left" : "right",
-        isFullWidth: true,
-        offsetMedia: false,
-        headerColor: "primary",
-        textColor: "black",
-        text: JSON.stringify(text),
-        media: [presetType.media],
-        addons: [],
-      };
-    });
-  };
-
+const Order = ({ layout, page }) => {
   return (
     <DefaultLayout layout={layout}>
-      <Page
-        page={page}
-        preSections={convertPresetTypesToSection(foodPresetTypes)}
-      />
+      <Page page={page} />
     </DefaultLayout>
   );
 };
@@ -71,13 +15,11 @@ export const getStaticProps = async ({ store }) => {
   const path = `/order`;
   const page = await API.getPage(encodeURI(path));
   const layout = await API.getLayoutData();
-  const foodPresetTypes = await API.getFoodPresetTypes();
 
   return {
     props: {
       layout,
       page,
-      foodPresetTypes,
     },
     revalidate: 1,
   };
