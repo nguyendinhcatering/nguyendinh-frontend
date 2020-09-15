a3cc6e5ed870b54ba6ed193c51d9580755e1d90e
Kevin Nguyen <k.nguyen0125@gmail.com>
2020-09-16 00:23:38 +0700

    fix: migrate to docker hub for builds

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
 
+console.log("public api", process.env.NEXT_PUBLIC_BACKEND_URL);
+
 module.exports = {
   webpack: (config) => {
     config.plugins.push(
