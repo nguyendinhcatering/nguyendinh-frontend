8482d6f02ac38611b7c62b1d94a8476c8a36a2d3
Kien Nguyen <k.nguyen0125@gmail.com>
2020-12-20 21:03:40 +0700

    feat: add facebook chat integration

--- components/layout/DefaultLayout/index.js
+++ components/layout/DefaultLayout/index.js
@@ -4,6 +4,7 @@ import Navbar from "../Navbar";
 import Footer from "../Footer";
 import DynamicBackground from "../../ui/DynamicBackground";
 import { SiteDataContext } from "../../ui/Map/SiteDataContext";
+import { CustomChat, FacebookProvider } from "react-facebook";
 
 const DefaultLayout = ({ layout, pullUp, children }) => {
   const { menus, footer, siteData } = layout || {
@@ -14,24 +15,36 @@ const DefaultLayout = ({ layout, pullUp, children }) => {
       mapOptions: [],
       mainPageUrl: "",
       apiPageUrl: "",
+      fbAppId: "fake-app-id",
+      fbPageId: "fake-page-id",
+      fbChatThemeColor: null,
+      fbChatGreetingMessage: null,
     },
   };
 
   return (
-    <SiteDataContext.Provider value={siteData}>
-      <div className="min-h-screen flex flex-col">
-        <Navbar menus={menus} />
-        <Box className="flex-grow relative overflow-hidden">
-          <DynamicBackground
-            images={siteData.backgroundImages}
-            pullUp={pullUp}
+    <FacebookProvider appId={siteData.fbAppId} chatSupport>
+      <SiteDataContext.Provider value={siteData}>
+        <div className="min-h-screen flex flex-col">
+          <Navbar menus={menus} />
+          <Box className="flex-grow relative overflow-hidden">
+            <DynamicBackground
+              images={siteData.backgroundImages}
+              pullUp={pullUp}
+            />
+            {children}
+            <Box className="flex-grow" />
+          </Box>
+          <CustomChat
+            pageId={siteData.fbPageId}
+            themeColor={siteData.fbChatThemeColor}
+            loggedInGreeting={siteData.fbChatGreetingMessage}
+            loggedOutGreeting={siteData.fbChatGreetingMessage}
           />
-          {children}
-          <Box className="flex-grow" />
-        </Box>
-        <Footer footer={footer} />
-      </div>
-    </SiteDataContext.Provider>
+          <Footer footer={footer} />
+        </div>
+      </SiteDataContext.Provider>
+    </FacebookProvider>
   );
 };
 
