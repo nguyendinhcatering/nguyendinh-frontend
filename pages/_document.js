938e1ba8893aac0fff1b818a747d22bbbb312476
nguyendinhcatering <79344166+nguyendinhcatering@users.noreply.github.com>
2023-07-12 16:02:28 +0700

    add gtm

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,8 @@ class MyDocument extends Document {
       <Html lang="vi">
         <Head></Head>
         <body>
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W5TTSM5');`}}></script>
+          <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W5TTSM5" height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
           <Main />
           <NextScript />
           {/*    <script*/}
